aa8f708b2b5c216ce52bc2fa51648e48b2399fa1
jieliuGG <810030709@qq.com>
2021-03-06 11:27:40 +0800

    commonBusiness

---
+++ pageobject/commonBusiness/nav_page.py
@@ -0,0 +1,12 @@
+# -- encoding: utf-8 --
+# @time:    	2021/1/31 10:38
+# @Author: 		jsonLiu
+# @Email:  		810030709@qq.com
+# @file: 		nav_page
+"""
+导航页面
+"""
+from python_AppTest.py_appTest_framework2.common.basepage import BasePage
+class NavPage(BasePage):
+    def switch_by_nav_name(self,nav_name):
+        pass
